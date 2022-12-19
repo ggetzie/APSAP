f899dad8e1f02a088533c025f08a26e483e99601
Gabriel Getzie <ggetzie@hku.hk>
2022-12-19 10:41:13 +0800

    formatting

--- mainWindow.py
+++ mainWindow.py
@@ -1,23 +1,42 @@
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtCore import Qt,QTimer
-from PyQt5.QtGui import QColor, QIcon, QPixmap, QImage, QWindow, QStandardItem, QStandardItemModel, QMovie, QPainter
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QListWidgetItem, QFileDialog, QMessageBox, QSplashScreen
- 
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import (
+    QColor,
+    QIcon,
+    QPixmap,
+    QImage,
+    QWindow,
+    QStandardItem,
+    QStandardItemModel,
+    QMovie,
+    QPainter,
+)
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    QApplication,
+    QWidget,
+    QListWidgetItem,
+    QFileDialog,
+    QMessageBox,
+    QSplashScreen,
+)
+
 import pathlib
 import open3d as o3d
 import re
 from database_tools import get_pottery_sherd_info, update_match_info
 from glob import glob as glob
- 
+
 import json
 import numpy as np
-basedir = pathlib.Path().resolve() 
+
+basedir = pathlib.Path().resolve()
 from components.vis import Visualized
 from components.pop_up import PopUp
 from components.load_qt_images_models import LoadImagesModels
- 
+
 FINDS_SUBDIR = "finds/individual"
 BATCH_3D_SUBDIR = "finds/3dbatch"
 FINDS_PHOTO_DIR = "photos"
@@ -25,43 +44,40 @@ MODELS_FILES_DIR = "finds/3dbatch/2022/batch_*/registration_reso1_maskthres242/f
 MODELS_FILES_RE = "finds/3dbatch/2022/batch_(.+?)/registration_reso1_maskthres242/final_output/piece_(.+?)_world.ply"
 HEMISPHERES = ("N", "S")
 
- 
-#Here let's do some simple machine learning to get
 
+# Here let's do some simple machine learning to get
 
 
 class MainWindow(QMainWindow, PopUp, Visualized, LoadImagesModels):
     """View (GUI)."""
-
 
     def __init__(self):
         """View initializer."""
         super(MainWindow, self).__init__()
 
         uic.loadUi("qtcreator/MainWindow.ui", self)
-        if not self.check_has_path_in_setting():        
+        if not self.check_has_path_in_setting():
             self.ask_for_prompt()
-        
-        setting = json.load(open("settings.json"))
-        self.file_root = pathlib.Path(setting["FILE_ROOT"] )
-#
 
+        setting = json.load(open("settings.json"))
+        self.file_root = pathlib.Path(setting["FILE_ROOT"])
+        #
 
         self.set_up_3d_window()
         self.populate_hemispheres()
- 
+
         self.hemisphere_cb.currentIndexChanged.connect(self.populate_zones)
         self.zone_cb.currentIndexChanged.connect(self.populate_eastings)
         self.easting_cb.currentIndexChanged.connect(self.populate_northings)
         self.northing_cb.currentIndexChanged.connect(self.populate_contexts)
         self.context_cb.currentIndexChanged.connect(self.contextChanged)
         self.contextDisplay.setText(self.get_context_string())
-        
-        
+
         self.findsList.currentItemChanged.connect(self.load_find_images)
-        
+
         self.update_button.clicked.connect(self.update_model_db)
         self.remove_button.clicked.connect(self.remove_match)
+
     def populate_hemispheres(self):
         self.hemisphere_cb.clear()
         res = [
@@ -141,10 +157,13 @@ class MainWindow(QMainWindow, PopUp, Visualized, LoadImagesModels):
         self.context_cb.addItems(res)
         self.set_context(0 if len(res) > 0 else -1)
         self.context_cb.setEnabled(len(res) > 1)
-        
-        funcs_to_run = [["Loading finds. It might take a while", self.populate_finds],["Loading models. It might take a while",self.populate_models]]
-        self.load_and_run(funcs_to_run) 
-    
+
+        funcs_to_run = [
+            ["Loading finds. It might take a while", self.populate_finds],
+            ["Loading models. It might take a while", self.populate_models],
+        ]
+        self.load_and_run(funcs_to_run)
+
     def set_context(self, index):
         self.context_cb.setCurrentIndex(index)
 
@@ -178,16 +197,14 @@ class MainWindow(QMainWindow, PopUp, Visualized, LoadImagesModels):
             return pathlib.Path()
         return res
 
-   
     def get_easting_northing_context(self):
         context_dir = self.get_context_dir()
-        path_parts = (pathlib.Path(context_dir).parts[-3:])
-        easting =  int(path_parts[0])
+        path_parts = pathlib.Path(context_dir).parts[-3:]
+        easting = int(path_parts[0])
         northing = int(path_parts[1])
         context = int(path_parts[2])
         return (easting, northing, context)
-  
- 
+
     def contextChanged(self):
         self.statusLabel.setText(f"")
         self.selected_find.setText(f"")
@@ -199,167 +216,198 @@ class MainWindow(QMainWindow, PopUp, Visualized, LoadImagesModels):
         if hasattr(self, "current_pcd"):
             self.vis.remove_geometry(self.current_pcd)
             self.current_pcd = None
-             
+
         model = QStandardItemModel(self)
         self.sortedModelList.setModel(model)
-        funcs_to_run = [["Loading finds. It might take a while", self.populate_finds],["Loading models. It might take a while",self.populate_models]]
+        funcs_to_run = [
+            ["Loading finds. It might take a while", self.populate_finds],
+            ["Loading models. It might take a while", self.populate_models],
+        ]
         import time
+
         now = time.time()
         self.load_and_run(funcs_to_run)
         print(f"Timed passed: {time.time() - now} seconds")
 
-        
-
     def populate_finds(self):
-        #if self.splash:
+        # if self.splash:
         #    self.splash.showMessage("Loading finds")
         self.findsList.clear()
         context_dir = self.get_context_dir()
         finds_dir = context_dir / FINDS_SUBDIR
         finds = [d.name for d in finds_dir.iterdir() if d.name.isdigit()]
-        #Getting easting, northing and context for getting doing the query
+        # Getting easting, northing and context for getting doing the query
         easting_northing_context = self.get_easting_northing_context()
         finds.sort(key=lambda f: int(f))
-        
-        #Get a dictionary to get all 
+
+        # Get a dictionary to get all
         self._3d_model_dict = dict()
         for find in finds:
             item = QListWidgetItem(find)
-            _3d_locations = get_pottery_sherd_info(easting_northing_context[0], easting_northing_context[1], easting_northing_context[2], int(find))
-          
-            
+            _3d_locations = get_pottery_sherd_info(
+                easting_northing_context[0],
+                easting_northing_context[1],
+                easting_northing_context[2],
+                int(find),
+            )
+
             if _3d_locations[0] != None and _3d_locations[1] != None:
                 item.setForeground(QColor("red"))
             self.findsList.addItem(item)
-            self._3d_model_dict[f"{easting_northing_context[0]},{easting_northing_context[1]},{easting_northing_context[2]},{int(find)}"] = _3d_locations
+            self._3d_model_dict[
+                f"{easting_northing_context[0]},{easting_northing_context[1]},{easting_northing_context[2]},{int(find)}"
+            ] = _3d_locations
 
     def populate_models(self):
-        #Populate all models and at the same time get the information of those models for comparison.
-          
-        path =  (str((self.get_context_dir()/MODELS_FILES_DIR)))
-        all_model_paths = (glob(path))
+        # Populate all models and at the same time get the information of those models for comparison.
+
+        path = str((self.get_context_dir() / MODELS_FILES_DIR))
+        all_model_paths = glob(path)
         if not all_model_paths:
             self.statusLabel.setText(f"No models were found")
-        #Setting up the model
+        # Setting up the model
         model = QStandardItemModel(self)
         model.setHorizontalHeaderLabels(["Models"])
-        #Getting a dict for all the batches
+        # Getting a dict for all the batches
         batches_dict = dict()
-        for  path in (all_model_paths):
-            m = re.search( MODELS_FILES_RE, path.replace("\\", "/"))
-            #This error happens when the relative path is different
+        for path in all_model_paths:
+            m = re.search(MODELS_FILES_RE, path.replace("\\", "/"))
+            # This error happens when the relative path is different
             batch_num = m.group(1)
             piece_num = m.group(2)
             if batch_num not in batches_dict:
                 batches_dict[batch_num] = [[int(piece_num), path]]
             else:
                 batches_dict[batch_num].append([int(piece_num), path])
-        #Sort the items
+        # Sort the items
         for key in batches_dict:
             batches_dict[key] = sorted(batches_dict[key])
-            
-        all_matched_3d_models = set()
- 
-        for key in (self._3d_model_dict):
-            if not (self._3d_model_dict[key][0] == None and self._3d_model_dict[key][1] == None):
-                all_matched_3d_models.add(self._3d_model_dict[key])
- 
-        
-        #Add all items onto the QTree
 
-        #We get all the information of all the 3d models, that we can use to rank them by similarity
+        all_matched_3d_models = set()
+
+        for key in self._3d_model_dict:
+            if not (
+                self._3d_model_dict[key][0] == None
+                and self._3d_model_dict[key][1] == None
+            ):
+                all_matched_3d_models.add(self._3d_model_dict[key])
+
+        # Add all items onto the QTree
+
+        # We get all the information of all the 3d models, that we can use to rank them by similarity
         actual_index = 0
-        all_3d_areas  = []
+        all_3d_areas = []
         all_3d_brightness_summaries = []
         all_3d_colors_summaries = []
         all_3d_width_length_summaries = []
         import time
+
         now = time.time()
         for batch in batches_dict:
-            items =  batches_dict[batch]
+            items = batches_dict[batch]
             batch = QStandardItem(f"{batch}")
-  
-            for item in  items:
+
+            for item in items:
                 index = item[0]
                 path = item[1]
-                m = re.search( MODELS_FILES_RE, path.replace("\\", "/"))
-                #This error happens when the relative path is different
+                m = re.search(MODELS_FILES_RE, path.replace("\\", "/"))
+                # This error happens when the relative path is different
                 batch_num = m.group(1)
                 piece_num = m.group(2)
- 
-                #Calculate the area of the current piece
-            
-               
-                brightness_summary = (self.comparator.get_brightness_summary_from_3d(path))
-                #Calculate the color summary of the current piece     
+
+                # Calculate the area of the current piece
+
+                brightness_summary = self.comparator.get_brightness_summary_from_3d(
+                    path
+                )
+                # Calculate the color summary of the current piece
                 colors_summary = self.comparator.get_color_summary_from_3d(path)
-           
-                area, width_length_summary = self.comparator.get_3d_object_area_and_width_length(path)
-            
+
+                (
+                    area,
+                    width_length_summary,
+                ) = self.comparator.get_3d_object_area_and_width_length(path)
+
                 all_3d_colors_summaries.append(colors_summary)
                 print(f"index: {actual_index}: 3dArea: {area}")
-                all_3d_areas.append([area, batch_num,piece_num ])
-                all_3d_brightness_summaries.append([brightness_summary, batch_num,piece_num ])
-                all_3d_width_length_summaries.append([width_length_summary, batch_num,piece_num ])
+                all_3d_areas.append([area, batch_num, piece_num])
+                all_3d_brightness_summaries.append(
+                    [brightness_summary, batch_num, piece_num]
+                )
+                all_3d_width_length_summaries.append(
+                    [width_length_summary, batch_num, piece_num]
+                )
                 ply = QStandardItem(f"{index}")
-                ply.setData(f"{path}", Qt.UserRole) 
+                ply.setData(f"{path}", Qt.UserRole)
                 if (int(batch_num), int(piece_num)) in all_matched_3d_models:
                     ply.setForeground(QColor("red"))
                 batch.appendRow(ply)
                 actual_index += 1
             model.appendRow(batch)
         print(f"{time.time() - now} seconds")
-        self.all_3d_areas = (all_3d_areas)
+        self.all_3d_areas = all_3d_areas
         self.all_3d_width_length_summaries = all_3d_width_length_summaries
         self.all_3d_brightness_summaries = all_3d_brightness_summaries
         self.all_3d_colors_summaries = all_3d_colors_summaries
         self.modelList.setModel(model)
         self.modelList.selectionModel().currentChanged.connect(self.change_3d_model)
-    
+
     def remove_match(self):
-        #This functions removes the match between the image and the 3d model
+        # This functions removes the match between the image and the 3d model
         selected_item = self.findsList.currentItem()
 
-        #Only remove model when an image is selected
+        # Only remove model when an image is selected
         if selected_item:
-            num = (selected_item.text())
+            num = selected_item.text()
             selected_item.setForeground(QColor("black"))
-            #Update the database
-            easting, northing, context =  (self.get_easting_northing_context())
-            update_match_info(easting,northing,context,num, None, None)
-            #Unred the matched items in the 3d models list
+            # Update the database
+            easting, northing, context = self.get_easting_northing_context()
+            update_match_info(easting, northing, context, num, None, None)
+            # Unred the matched items in the 3d models list
             previous_current_batch_num = self.current_batch.text()
             previous_current_piece_num = self.current_piece.text()
- 
+
             mod = self.modelList.model()
-            #Make the item Black in modelList
+            # Make the item Black in modelList
             for i in range(mod.rowCount()):
                 for j in range(mod.item(i).rowCount()):
-                        if previous_current_batch_num != "NS" and previous_current_piece_num != "NS":
-                            if int(previous_current_batch_num) == int(mod.item(i).text()) and int(previous_current_piece_num) == int(mod.item(i).child(j).text()):
-                                #Make the old selected black
-                                mod.item(i).child(j).setForeground(QColor("black"))
-            #Make the item Black in model sorted list
+                    if (
+                        previous_current_batch_num != "NS"
+                        and previous_current_piece_num != "NS"
+                    ):
+                        if int(previous_current_batch_num) == int(
+                            mod.item(i).text()
+                        ) and int(previous_current_piece_num) == int(
+                            mod.item(i).child(j).text()
+                        ):
+                            # Make the old selected black
+                            mod.item(i).child(j).setForeground(QColor("black"))
+            # Make the item Black in model sorted list
             sortedMod = self.sortedModelList.model()
             for i in range(sortedMod.rowCount()):
 
-                    if previous_current_batch_num != "NS" and previous_current_piece_num != "NS":        
-                        
-                        if sortedMod.item(i).text() == f"Batch {int(previous_current_batch_num):03}, model: {int(previous_current_piece_num)}":       
-                                
-                            (sortedMod.item(i)).setForeground(QColor("black"))
-                #Also we need to unred the previous selected item in the sorted model list            
- 
-            #Remove the dict entry from 
-            dict_key = f"{easting},{northing },{context},{int(num)}" 
+                if (
+                    previous_current_batch_num != "NS"
+                    and previous_current_piece_num != "NS"
+                ):
+
+                    if (
+                        sortedMod.item(i).text()
+                        == f"Batch {int(previous_current_batch_num):03}, model: {int(previous_current_piece_num)}"
+                    ):
+
+                        (sortedMod.item(i)).setForeground(QColor("black"))
+            # Also we need to unred the previous selected item in the sorted model list
+
+            # Remove the dict entry from
+            dict_key = f"{easting},{northing },{context},{int(num)}"
             self._3d_model_dict[dict_key] = (None, None)
             self.current_batch.setText(f"NS")
-            self.current_piece.setText(f"NS")          
-        
-            #Remove the current 3d model
+            self.current_piece.setText(f"NS")
+
+            # Remove the current 3d model
 
 
- 
 def main():
     """Main function."""
     # Create an instance of QApplication

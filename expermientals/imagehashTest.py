0f1687aa7ce278b8e8fec34be85514b46c8bd63c
Bert Liu <bertliu@hku.hk>
2023-02-16 17:10:52 +0800

    Simiplifiny the summary of pixels

--- expermientals/imagehashTest.py
+++ expermientals/imagehashTest.py
@@ -1,3 +1,4 @@
+
 import ctypes
 
 opengl_path = "../computation/opengl32.dll"
@@ -9,50 +10,24 @@ import imagehash
 import sys
 sys.path.insert(0,'..')
 from computation.nn_segmentation import MaskPredictor
-def white_to_transparency(img):
-    x = np.asarray(img.convert('RGBA')).copy()
+import matplotlib.pyplot as plt
 
-    x[:, :, 3] = (255 * (x[:, :, :3] <= 240).any(axis=2)).astype(np.uint8)
-
-    return Image.fromarray(x)
-
-
-ply_window = o3d.visualization.Visualizer()
-
-ply_window.create_window(visible=False)   
-ply_window.get_render_option().light_on = False
-model_path = r"D:\ararat\data\files\N\38\478020\4419550\11\finds\3dbatch\2022\batch_000\registration_reso1_maskthres242\final_output\piece_0_world.ply"
-current_pcd_load = o3d.io.read_point_cloud(model_path) 
-
-ply_window.add_geometry(current_pcd_load)
-
-ctr = ply_window.get_view_control()    
-
-ply_window.get_render_option().point_size = 5
-ctr.change_field_of_view(step=-9)
-
-object_image = ply_window.capture_screen_float_buffer(True)
-pic =   white_to_transparency( Image.fromarray(np.multiply(np.array(object_image), 255).astype(np.uint8)).resize((450, 300), Image.LANCZOS))
-pic.save("here.png")
-print(imagehash.average_hash(pic))
-
-
-pic_front = r"D:\ararat\data\files\N\38\478020\4419550\11\finds\individual\1\photos\1.jpg"
-back_front = r"D:\ararat\data\files\N\38\478020\4419550\11\finds\individual\1\photos\2.jpg"
-
-
-
-pic_f_image = Image.open(pic_front).resize((450, 300), Image.LANCZOS)
  
-ceremicsmask_predictor = MaskPredictor("../computation/ceremicsmask.pt")
-
-mask_image_np = np.array(ceremicsmask_predictor.predict(pic_f_image))
+    
 
 
-def get_transparent_background_sherd(img, img_mask):
+model_path = r"D:\ararat\data\files\N\38\478020\4419550\11\finds\3dbatch\2022\batch_000\registration_reso1_maskthres242\final_output\piece_0_world.ply"
 
-    img_np = np.array(img.convert('RGBA'))
-    img_mask_np = np.array(img_mask)
+
+
+
+def get_lumance_summary_2d(img_path):
+    pic_f_image = Image.open(img_path).resize((450, 300), Image.LANCZOS)
+    ceremicsmask_predictor = MaskPredictor("../computation/ceremicsmask.pt")
+    #Getting the numpy arrys for the image and the mask
+    img_mask_np = np.array(ceremicsmask_predictor.predict(pic_f_image))
+    img_np = np.array(pic_f_image.convert('RGBA'))
+    #Cleaning the background
     for i in range(len(img_mask_np)):
         for j in range(len(img_mask_np[0])):
            if img_mask_np[i][j]  <= 170:
@@ -60,24 +35,73 @@ def get_transparent_background_sherd(img, img_mask):
                 img_np[i][j][1] = 0
                 img_np[i][j][2] = 0
                 img_np[i][j][3] = 0
-    return Image.fromarray(img_np)
-img1 = (get_transparent_background_sherd(pic_f_image, mask_image_np))#.save("pic_f_image.png")
-print(imagehash.average_hash(img1))
+    image =   Image.fromarray((img_np))
 
+    np_image =np.array(image.convert('L')).ravel()
+    
+    #gather lumance summary
+    lumance_summary = []
+ 
+    for i in range(0, 256):
+        lumance_summary.append(0)
+    for luminance in np_image:
+        if luminance != 0:
+            lumance_summary[luminance] += 1
+ 
+ 
+    return lumance_summary
+
+
+def get_lumance_summary_3d(model_path):
+    ply_window = o3d.visualization.Visualizer()
+
+    ply_window.create_window(visible=False)   
+    ply_window.get_render_option().light_on = False
+    #load the model
+    current_pcd_load = o3d.io.read_point_cloud(model_path) 
+    ply_window.add_geometry(current_pcd_load)
+    ctr = ply_window.get_view_control()    
+    ply_window.get_render_option().point_size = 5
+    ctr.change_field_of_view(step=-9)
+
+    img_captured = ply_window.capture_screen_float_buffer(True)
+    image_np =  np.multiply(np.array(object_image), 255).astype(np.uint8)
+    img = Image.fromarray(image_np)
+
+    img_np = np.array(img.convert('RGBA'))
+    
+    for i in range(len(img_np)):
+        for j in range(len(img_np[0])):
+           if img_np[i][j][0]  == 255 and img_np[i][j][1]  == 255 and img_np[i][j][2]  == 255:
+                img_np[i][j][0] = 0
+                img_np[i][j][1] = 0
+                img_np[i][j][2] = 0
+                img_np[i][j][3] = 0
+    image =   Image.fromarray((img_np))
+
+    np_image = np.array(image.convert('L')).ravel()
  
 
+    #clean up the model
 
+    ply_window.remove_geometry(current_pcd_load)
+    del ctr
 
-pic_b_image = Image.open(back_front).resize((450, 300), Image.LANCZOS)
+    #generate the summary and return the result
+ 
+    
+    #gather lumance summary
+    lumance_summary = {}
+    for i in range(0, 256):
+        lumance_summary[i] = 0
+    for luminance in np_image:
+        if luminance != 0:
+            lumance_summary[luminance] += 1
+    return lumance_summary
 
-mask_image_np_b = np.array(ceremicsmask_predictor.predict(pic_b_image))
-img2 = (get_transparent_background_sherd(pic_b_image, mask_image_np_b))#.save("pic_b_image.png")
-print(imagehash.average_hash(img2))
-
-#mask_image.save("pic_f_image.png")
-#(white_to_transparency(pic)).save("here.png")
-#pic[pic==255] = 0
-#ply_window.remove_geometry(current_pcd_load)
-
-#del ctr
-#ply_window.destroy_window()
+all_images_urls = []
+ceremicsmask_predictor = MaskPredictor("../computation/ceremicsmask.pt")
+img_path = r"D:\ararat\data\files\N\38\478020\4419550\11\finds\individual\1\photos\1.jpg"
+    
+print(sum(get_lumance_summary_2d(img_path)))
+ 

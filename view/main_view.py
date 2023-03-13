db502cd246aa438a9b2d7b302c3e484fce8476a4
Bert Liu <bertliu@hku.hk>
2023-03-13 14:35:34 +0800

    First step of improving gui done

--- view/main_view.py
+++ view/main_view.py
@@ -67,7 +67,7 @@ class MainView(QMainWindow, PlyWindowMixin):
         main_view.finds_list.currentItemChanged.connect(main_presenter.load_find_images)
         main_view.update_button.clicked.connect(main_presenter.add_match)
         main_view.remove_button.clicked.connect(main_presenter.remove_match)
-        main_view.update_weights_button.clicked.connect(main_view.get_features_weights)
+      #  main_view.update_weights_button.clicked.connect(main_view.get_features_weights)
 
         main_view.hemisphere_cb.currentIndexChanged.connect(
             main_presenter.populate_zones
@@ -85,21 +85,21 @@ class MainView(QMainWindow, PlyWindowMixin):
         main_view = self
 
         initial_weights = {'area_similarity': 1.0, 'brightness_similarity': 0.84, 'brightness_std_similarity': 0.21, 'width_length_similarity': 0.46,   'extra_similarities': 0.08}
-        main_view.areaSlider.setValue(initial_weights["area_similarity"]*100)
-        main_view.brightnessSlider.setValue(initial_weights["brightness_similarity"]*100)
-        main_view.brightnessStdSlider.setValue(initial_weights["brightness_std_similarity"]*100)
-        main_view.widthLengthSlider.setValue(initial_weights["width_length_similarity"]*100)
-        main_view.identifierSlider.setValue(initial_weights["extra_similarities"]*100)
+      #  main_view.areaSlider.setValue(initial_weights["area_similarity"]*100)
+      #  main_view.brightnessSlider.setValue(initial_weights["brightness_similarity"]*100)
+      #  main_view.brightnessStdSlider.setValue(initial_weights["brightness_std_similarity"]*100)
+      #  main_view.widthLengthSlider.setValue(initial_weights["width_length_similarity"]*100)
+      #  main_view.identifierSlider.setValue(initial_weights["extra_similarities"]*100)
     def get_features_weights(self):
         main_view = self
  
         weights = {
-                "area_similarity": int(main_view.areaSlider.value())/100 ,
-                "brightness_similarity": int(main_view.brightnessSlider.value())/100,
-                "brightness_std_similarity": int(main_view.brightnessStdSlider.value())/100,
-                "width_length_similarity" : int(main_view.widthLengthSlider.value())/100,
+                "area_similarity": 0.2,#int(main_view.areaSlider.value())/100 ,
+                "brightness_similarity": 0.2,#int(main_view.brightnessSlider.value())/100,
+                "brightness_std_similarity": 0.2,#int(main_view.brightnessStdSlider.value())/100,
+                "width_length_similarity" : 0.2,#int(main_view.widthLengthSlider.value())/100,
                 
-                "extra_similarities": int(main_view.identifierSlider.value())/100
+                "extra_similarities": 0.2,#int(main_view.identifierSlider.value())/100
         }
         main_view.weights = weights
         print(weights)

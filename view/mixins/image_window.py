be634f320c6a0bcaee1da1ea1b0216a5abfb4f39
Gabriel Getzie <gabe@kotsf.com>
2024-12-23 11:04:34 +0800

    load larger images on click

--- view/mixins/image_window.py
+++ view/mixins/image_window.py
@@ -1,29 +1,40 @@
-from PyQt5.QtWidgets import (
-    QLabel,
-    QVBoxLayout,
-    QWidget,
-)
+import logging
+from pathlib import Path
+from PIL import Image
+from PIL.ImageQt import ImageQt
+from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget
 from PyQt5.QtGui import QPixmap
+
+logger = logging.getLogger(__name__)
 
 
 class ImageWindow(QWidget):
     """This QWidget is shown when a person click on findFrontPhoto_l or findBackPhoto_l"""
 
-    def __init__(self, current_image_path):
+    def __init__(self, current_image_path: str):
         super().__init__()
-
-        current_view = self
-        current_view.label = QLabel(f"{current_image_path}")
-
+        current = Path(current_image_path)
+        self.setWindowTitle(f"Image: {current.stem}")
+        larger = current.parent / f"{current.stem}-1500{current.suffix}"
+        self.image_path = larger if larger.exists() else current
+        self.label = QLabel(f"{current_image_path}")
+        logger.debug("current_image_path: %s", current_image_path)
+        try:
+            photo: Image.Image = Image.open(str(self.image_path))
+        except (FileNotFoundError, IOError, OSError, TypeError, ValueError) as e:
+            logger.error("Error opening photo at %s: %s", self.image_path, e)
+            return
+        logger.debug("loaded photo from : %s", self.image_path)
         # 1. Loading from the image path to the pixmap, then to the label.
-        pixmap = QPixmap(current_image_path)
-        pixmap = pixmap.scaledToWidth(720)
-        current_view.label.setPixmap(pixmap)
+        im_qt = ImageQt(photo)
+        pixmap = QPixmap.fromImage(im_qt)
+        logger.debug("photo width: %s", photo.width)
+        self.label.setPixmap(pixmap.scaledToWidth(720))
 
         # 2. Loading from the image label to the layout.
         layout = QVBoxLayout()
-        layout.addWidget(current_view.label)
-        current_view.setLayout(layout)
+        layout.addWidget(self.label)
+        self.setLayout(layout)
 
 
 class OpenImageMixin:
